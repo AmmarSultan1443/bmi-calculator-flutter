83962d689ee4687998ef359ed654e6921d077650
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-02-01 16:22:15 +0100

    Top left card appearance is done.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
 const bottomContainerColor = Color(0xFFEB1555);
+const childCardTextColor = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,52 +15,71 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(colour: activeCardColor),
-                ),
-                Expanded(
-                  child: ReusableCard(colour: activeCardColor),
-                ),
-              ],
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.mars,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: childCardTextColor
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ReusableCard(colour: activeCardColor),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(colour: activeCardColor),
-                ),
-                Expanded(
-                  child: ReusableCard(colour: activeCardColor),
-                ),
-              ],
+            Expanded(
+              child: ReusableCard(colour: activeCardColor),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10.0),
-            height: bottomContainerHeight,
-            width: double.infinity,
-            color: bottomContainerColor,
-          )
-        ],
-      )
-    );
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10.0),
+              height: bottomContainerHeight,
+              width: double.infinity,
+              color: bottomContainerColor,
+            )
+          ],
+        ));
   }
 }
 
 class ReusableCard extends StatelessWidget {
-
   final Color colour;
   final cardChild;
 
@@ -69,11 +90,9 @@ class ReusableCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: cardChild,
-    margin: EdgeInsets.all(15.0),
-    decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0)
-    ),
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
